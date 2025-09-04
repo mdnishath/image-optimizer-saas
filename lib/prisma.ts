b9e7b4bc37fd2fe22641065fe74996f70bdaedfd
mdnishath <nishatbd3388@gmail.com>
2025-09-05 04:50:30 +0600

    prisma next

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,22 +1,5 @@
-import { PrismaClient } from "@/lib/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export default prisma;
-
-// const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-// export const prisma =
-//   globalForPrisma.prisma ||
-//   new PrismaClient({
-//     datasources: {
-//       db: {
-//         url: process.env.DATABASE_URL as string, // Works in local + production
-//       },
-//     },
-//     log: ["error", "warn"], // Add "query" for debugging
-//   });
-
-// if (process.env.NODE_ENV !== "production") {
-//   globalForPrisma.prisma = prisma;
-// }
