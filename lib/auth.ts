cd02bdbe4ceb7f2ba44b84c6c54882c45dc6fd62
mdnishath <nishatbd3388@gmail.com>
2025-09-05 04:36:05 +0600

    update

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,4 @@
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 import jwt, { JwtPayload } from "jsonwebtoken";
 
 export async function getUserFromRequest(req: Request) {
