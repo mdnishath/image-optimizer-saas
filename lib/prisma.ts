e70f883b34d40a745a47dbe9659f40fc7cd380e5
mdnishath <nishatbd3388@gmail.com>
2025-09-05 05:47:24 +0600

    DB Fixed

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,14 @@
-// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error", "warn"], // add "query" for debugging if needed
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
